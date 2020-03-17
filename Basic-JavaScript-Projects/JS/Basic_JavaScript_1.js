4bf568322243ab7d4c4a339dba3266d0a911f1ff
stephaniecadd <stephaniecadd94@gmail.com>
2020-03-16 18:02:40 -0700

    Basic-JavaScript

---
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+var dashund = "Rex", lhasaapso= "Toby", Aussie= "Jax";
+var dashund = dashund.fontcolor("Red");
+var lhasaapso = lhasaapso.fontcolor("Blue");
+var Aussie = Aussie.fontcolor("Yellow")
+document.write(lhasaapso);
+document.write(4+25);
