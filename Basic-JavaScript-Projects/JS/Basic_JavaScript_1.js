7fde327f1ce004de07d9f0ff403ade95ebfe7ad9
stephaniecadd <stephaniecadd94@gmail.com>
2020-03-16 18:07:29 -0700

    Update Code

--- Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
@@ -1,6 +1,8 @@
-var dashund = "Rex", lhasaapso= "Toby", Aussie= "Jax";
-var dashund = dashund.fontcolor("Red");
-var lhasaapso = lhasaapso.fontcolor("Blue");
-var Aussie = Aussie.fontcolor("Yellow")
-document.write(lhasaapso);
+var Dashund = "Rex", Dalmation= "Toby", Shepard= "Jax";
+var Dashund = Dashund.fontcolor("Red");
+var Dalmation = Dalmation.fontcolor("Blue");
+var Shepard = Shepard.fontcolor("Yellow")
+document.write("We have a Dalmation with this name: ");
+document.write(Dalmation );
+document.write("There are this many dogs: ")
 document.write(4+25);
