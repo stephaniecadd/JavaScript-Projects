29d8e726b04727c89f3a6c8e0707782a01e8be11
Stephanie Cadd <stephaniecadd94@gmail.com>
2020-03-26 11:03:33 -0700

    TicTacToe

--- JavaScript/JavaScript_3.js
+++ JavaScript/JavaScript_3.js
@@ -1,4 +1,4 @@
 function displayType(character) {
     var characterType = character.getAttribute("data-character-type");
-    alert(characterType + " is in " + character.innerHTML);
+    alert(characterType + " is in a " + character.innerHTML);
 }
