521340014400be6d06751c1a44ad575a57928d36
stephaniecadd <stephaniecadd94@gmail.com>
2020-04-02 09:01:59 -0700

    Add Assignments

--- JavaScript/JavaScript_2.js
+++ JavaScript/JavaScript_2.js
@@ -5,4 +5,18 @@ function validateform() {
         return false;
     }
 }
-
+function openForm() {
+    document.getElementById("myForm2").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("myForm2").style.display = "none";
+}
+$("slideshow > div:gt(0)").hide();
+setInterval(function() {
+    $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
