03083e0a7160bd742dc28d458fe10d94bf58cb3b
stephaniecadd <stephaniecadd94@gmail.com>
2020-05-26 16:05:55 -0700

    Organize Files

--- Basic-JavaScript-Projects/JS/Basic_JavaScript_1.js
+++
@@ -1,8 +0,0 @@
-var Dashund = "Rex", Dalmation= "Toby", Shepard= "Jax";
-var Dashund = Dashund.fontcolor("Red");
-var Dalmation = Dalmation.fontcolor("Blue");
-var Shepard = Shepard.fontcolor("Yellow")
-document.write("We have a Dalmation with this name: ");
-document.write(Dalmation );
-document.write("There are this many dogs: ")
-document.write(4+25);
--- Basic-JavaScript-Projects/JS/Basic_JavaScript_2.js
+++
@@ -1,8 +0,0 @@
-function My_First_Function () {
-    var sentence = "This text is green!";
-    sentence +=" No, it is not.";
-    document.getElementById("Green_Text").innerHTML = sentence;
-}
-function Second_Function () {
-    document.getElementById("Hello").innerHTML = "Hello";
-}
